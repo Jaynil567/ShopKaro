48913f13d346a14a55e11868a22fb8265f9d9cec
Jaynil Bhalani <bhalanijaynil@gmail.com>
2026-03-19 11:08:39 +0530

    Update ShopKaro.py

--- ShopKaro.py
+++ ShopKaro.py
@@ -479,8 +479,9 @@ def Mediator_Portal_Dashboard():
 
     TO=0
     for row in data_rows:
-        TO+=1
-        user_orders.append((row[order_id_index], row[order_date_index], row[order_status_index], row[order_brand_index], row[order_refundAmount_index],row[order_reviewer_index], row[order_date_index], row[mobile_index],row[timestamp_index], row[order_ss_index]))
+        if row[order_status_index]:
+            TO+=1
+            user_orders.append((row[order_id_index], row[order_date_index], row[order_status_index], row[order_brand_index], row[order_refundAmount_index],row[order_reviewer_index], row[order_date_index], row[mobile_index],row[timestamp_index], row[order_ss_index]))
 
     
     CO=0
